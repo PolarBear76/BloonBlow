a67d7293e6fe42059a48dc6adbdd423c9ec1c57c
Deathnei <axel.kaliff@gmail.com>
2016-04-08 23:21:25 +0200

    added glyphicons, added pages for devise and forms for these

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :installs
+  devise_for :users 
+  
   get 'pages/home'
   get 'about' => 'pages#about'
+ 
   root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
