370c4ed95e99f9b097119bec2330606dedcff96d
Axel Kaliff <axel.kaliff@gmail.com>
2016-04-13 19:17:22 +0200

    add paperclip gem

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,46 +1,31 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, except: [:index]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /pins
-  # GET /pins.json
   def index
-    @pins = Pin.all
+    @pins = Pin.all.order("created_at DESC")
   end
 
-  # GET /pins/1
-  # GET /pins/1.json
   def show
   end
 
-  # GET /pins/new
   def new
-    @pin = current_user.pins.new
+    @pin = current_user.pins.build
   end
 
-  # GET /pins/1/edit
   def edit
-    @pin = current_user.pins.find(params[:id])
   end
 
-  # POST /pins
-  # POST /pins.json
   def create
     @pin = current_user.pins.new(pin_params)
-
-    respond_to do |format|
-      if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-        format.json { render :show, status: :created, location: @pin }
-      else
-        format.html { render :new }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
-  # PATCH/PUT /pins/1
-  # PATCH/PUT /pins/1.json
   def update
     if @pin.update(pin_params)
       redirect_to @pin, notice: 'Pin was successfully updated.'
@@ -49,15 +34,9 @@ class PinsController < ApplicationController
     end
   end
 
-  # DELETE /pins/1
-  # DELETE /pins/1.json
   def destroy
-    @pin = current_user.pins.find(params[:id])
     @pin.destroy
-    respond_to do |format|
-      format.html { redirect_to pins_url, notice: 'Pin was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to pins_url
   end
 
   private
@@ -66,8 +45,13 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,8 +1,9 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	
-	
 	validates :description, presence: true
 
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+validates :image, presence: true
 end
