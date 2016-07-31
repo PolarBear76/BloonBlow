9ec0c5880a953ae6dae83ed4d185eb6bc5f61d21
Axel Kaliff <axel.kaliff@gmail.com>
2016-07-31 19:40:28 +0200

    altered pins controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -51,10 +51,6 @@ def downvote
   redirect_to pins_path
 end
 
-def self.score
-  self.get_upvotes.size - self.get_downvotes.size
-end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
