c40904edbfe67388e66984ea12e554fdf5c52573
Axel Kaliff <axel.kaliff@gmail.com>
2016-08-01 08:58:12 +0200

    fixed error(?) by removing gem version specs for paperclip and aws-sdk

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,4 +4,5 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://www.penguinstats.com/images/not-available.png"
   	validates :image, presence: true, file_size: { less_than: 2.gigabytes }
  	validates :description, presence: true
+ 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
